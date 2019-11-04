022bfc77374fe5987483cf1110d98534284ee281
vale75020 <valentinosuma@hotmail.com>
2019-11-04 17:25:06 +0100

    joi

--- models/user.model.js
+++ models/user.model.js
@@ -1,19 +1,39 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const Mongoose = require("mongoose");
+const Joigoose = require("joigoose")(Mongoose);
+const Joi = require("joi");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  admin: {
-    type: Boolean,
-    default: false
-  }
+var joiUserSchema = Joi.object({
+  username: Joi.string()
+    .min(4)
+    .max(30)
+    .required(),
+  password: Joi.string()
+    .min(4)
+    .max(100)
+    .required(),
+  admin: Joi.boolean().meta({ default: false })
 });
 
-module.exports = User = mongoose.model("users", userSchema);
+var mongooseUserSchema = new Mongoose.Schema(Joigoose.convert(joiUserSchema));
+
+module.exports = User = Mongoose.model("users", mongooseUserSchema);
+ 
+// const mongoose = require("mongoose");
+// const { Schema } = mongoose;
+
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   admin: {
+//     type: Boolean,
+//     default: false
+//   }
+// });
+
+// module.exports = User = mongoose.model("users", userSchema);
